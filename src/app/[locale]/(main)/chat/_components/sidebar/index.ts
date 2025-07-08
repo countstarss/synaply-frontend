dba6bf989ef32df325b33604f3907864988a946a
Luke <countstarss@163.com>
2025-07-08 13:52:40 +0800

    chore: pnpm build

--- src/app/[locale]/(main)/chat/_components/sidebar/index.ts
+++ src/app/[locale]/(main)/chat/_components/sidebar/index.ts
@@ -7,11 +7,3 @@ export { ContactItem } from "./ContactItem";
 export { ChatList } from "./ChatList";
 export { ContactList } from "./ContactList";
 export * from "./types";
-
-// 重新导出聊天相关类型
-export type {
-  Chat,
-  CreateGroupChatData,
-  CreatePrivateChatData,
-  ChatResponse,
-} from "@/lib/fetchers/chat";
